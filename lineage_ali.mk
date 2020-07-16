f153ac9f7a0c963981544b3e73168614363a2a0b
betoxxD <40176112+betoxxD@users.noreply.github.com>
2020-07-15 20:14:16 -0500

    Adding Boot animation res

--- lineage_ali.mk
+++ lineage_ali.mk
@@ -30,6 +30,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ali
