420faf020325fe6a3c69a5f46863f9420afab119
betoxxD <40176112+betoxxD@users.noreply.github.com>
2020-07-25 13:20:00 -0500

    Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ali.mk
+    $(LOCAL_DIR)/havoc_ali.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ali-user \
-    lineage_ali-userdebug \
-    lineage_ali-eng
+    havoc_ali-user \
+    havoc_ali-userdebug \
+    havoc_ali-eng
